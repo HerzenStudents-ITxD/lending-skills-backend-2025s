55b15fdee5bb6d5e1e1e3615f10687c4cb3a033a
alis13nya <166594142+alis13nya@users.noreply.github.com>
2025-04-01 22:58:32 +0300

    backend

---
+++ lending_skills_backend/lending_skills_backend/Repositories/BlocksRepository.cs
@@ -0,0 +1,50 @@
+﻿using lending_skills_backend.DataAccess;
+using lending_skills_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace lending_skills_backend.Repositories
+{
+    public class BlocksRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlocksRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<DbBlock>> GetBlocksAsync()
+        {
+            return await _context.Blocks.ToListAsync();
+        }
+
+        public async Task<DbBlock?> GetBlockByIdAsync(int id)
+        {
+            return await _context.Blocks.FindAsync(id);
+        }
+
+        public async Task AddBlockAsync(DbBlock block)
+        {
+            _context.Blocks.Add(block);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBlockAsync(DbBlock block)
+        {
+            _context.Blocks.Update(block);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBlockAsync(int id)
+        {
+            var block = await _context.Blocks.FindAsync(id);
+            if (block != null)
+            {
+                _context.Blocks.Remove(block);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ lending_skills_backend/lending_skills_backend/Repositories/Professors_programsRepository.cs
@@ -0,0 +1,41 @@
+﻿using lending_skills_backend.DataAccess;
+using lending_skills_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lending_skills_backend.Repositories;
+
+public class ProfessorsProgramsRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ProfessorsProgramsRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<DbProfessorProgram>> GetProfessorsProgramsAsync()
+    {
+        return await _context.ProfessorsPrograms.ToListAsync();
+    }
+
+    public async Task<DbProfessorProgram?> GetProfessorProgramByIdAsync(int professorId, int programId)
+    {
+        return await _context.ProfessorsPrograms.FindAsync(professorId, programId);
+    }
+
+    public async Task AddProfessorProgramAsync(DbProfessorProgram professorProgram)
+    {
+        _context.ProfessorsPrograms.Add(professorProgram);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoveProfessorProgramAsync(int professorId, int programId)
+    {
+        var professorProgram = await _context.ProfessorsPrograms.FindAsync(professorId, programId);
+        if (professorProgram != null)
+        {
+            _context.ProfessorsPrograms.Remove(professorProgram);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
